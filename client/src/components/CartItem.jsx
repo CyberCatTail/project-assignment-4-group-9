3802cf40e05a025e95e916748b31b5200c1f691b
yumeng <yzha5882@uwo.ca>
2024-11-23 21:53:12 -0500

    feat: change svg to icon

--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,29 +1,12 @@
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import { PlusIcon, MinusIcon, TrashIcon } from "@heroicons/react/24/outline";
+
 
 import {ParsePrice} from "@/lib/utils"
 
 function CartItem({product, ...props}) {
 
   return (
-    <div>
+    <div {...props}>
         <div className="flex items-center gap-4">
             <div className="w-24 h-24 shrink-0 bg-white p-2 rounded-md">
                 <img src={product.img} className="w-full h-full object-contain" />
@@ -38,22 +21,14 @@ function CartItem({product, ...props}) {
                     <div>
                         <button type="button"
                             className="flex items-center px-2.5 py-1.5 border border-gray-300 text-gray-800 text-xs outline-none bg-transparent rounded-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-2.5 fill-current" viewBox="0 0 124 124">
-                                <path d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z" data-original="#000000"></path>
-                            </svg>
-
+                            <MinusIcon className="size-3 stroke-[3px]"/>
                             <span className="mx-2.5">{product.quantity}</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-2.5 fill-current" viewBox="0 0 42 42">
-                                <path d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z" data-original="#000000"></path>
-                            </svg>
+                            <PlusIcon className="size-3 stroke-[3px]"/>
                         </button>
                     </div>
 
                     <div className="ml-auto">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-4 fill-red-500 inline cursor-pointer" viewBox="0 0 24 24">
-                            <path d="M19 7a1 1 0 0 0-1 1v11.191A1.92 1.92 0 0 1 15.99 21H8.01A1.92 1.92 0 0 1 6 19.191V8a1 1 0 0 0-2 0v11.191A3.918 3.918 0 0 0 8.01 23h7.98A3.918 3.918 0 0 0 20 19.191V8a1 1 0 0 0-1-1Zm1-3h-4V2a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v2H4a1 1 0 0 0 0 2h16a1 1 0 0 0 0-2ZM10 4V3h4v1Z" data-original="#000000"></path>
-                            <path d="M11 17v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Zm4 0v-7a1 1 0 0 0-2 0v7a1 1 0 0 0 2 0Z" data-original="#000000"></path>
-                        </svg>
+                        <TrashIcon className="size-4 stroke-2 stroke-red-500 inline cursor-pointer"/>
                     </div>
                 </div>
             </div>
--- client/src/components/PaymentCard.jsx
+++ client/src/components/PaymentCard.jsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { CreditCardIcon } from "@heroicons/react/24/outline";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
@@ -39,7 +40,7 @@ function PaymentCard({products, ...props}) {
   Total    = ParsePrice(Total);
  
   return (
-    <Card>
+    <Card {...props}>
       <CardHeader>
         <CardTitle>Payment Method</CardTitle>
         <CardDescription>
@@ -54,19 +55,7 @@ function PaymentCard({products, ...props}) {
               htmlFor="card"
               className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="mb-3 h-6 w-6"
-              >
-                <rect width="20" height="14" x="2" y="5" rx="2" />
-                <path d="M2 10h20" />
-              </svg>
+              <CreditCardIcon className="size-12"/>
               Card
             </Label>
           </div>
