d5edb5110a0d61382961d40d33ca945628158d96
QiujuDai <qdai43@uwo.ca>
2024-12-02 16:34:09 -0500

    add add2cart button & review data

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -7,25 +7,40 @@ import {
 } from "@/components/ui/card";
 import { ParsePrice } from "@/lib/utils";
 import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button"
+import { addToCart } from "@/api/userApi";
 
 function ProductCard({ product, ...props }) {
+
+  const AddToCart = async () => {
+    await addToCart(product.product_id, 1)
+  }
+
   return (
     <Card {...props}>
       <Link to={`/product/${product.product_id}`}>
-        <CardHeader></CardHeader>
-        <CardContent className="flex justify-center items-center h-[150px]">
-          
+        <CardContent className="items-center h-[150px]">
             <img
               src={product.img}
               alt={`${product.brand} ${product.model}`}
-              className="max-h-full max-w-full object-contain"
+              className="h-full w-full"
             />
         </CardContent>
-        <CardFooter className="flex flex-col items-start">
-          <p>{`${product.brand} ${product.model}`}</p>
-          <p>{ParsePrice(product.price)}</p>
+        
+        <CardFooter className="flex space-y-4 flex-col items-start">
+          <div className = "w-full h-22">
+            <p>{`${product.brand} ${product.model}`}</p>
+            <p className = "font-bold">{ParsePrice(product.price)}</p>
+          </div>
+
+             
         </CardFooter>
       </Link>
+      <div className = "w-full justify-items-center ">
+          <Button  className = "mx-auto block mb-2" onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}>
+              Add to Cart
+          </Button>
+      </div>    
     </Card>
   );
 }
